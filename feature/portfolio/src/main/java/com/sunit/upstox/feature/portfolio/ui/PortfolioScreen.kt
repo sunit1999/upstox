110a323d3385e76ef179e71d0f59e1dba61ca2a1
sunit <sunitsharma1999@gmail.com>
2024-03-28 19:12:51 +0530

    Added Portfolio Feature Module

---
+++ feature/portfolio/src/main/java/com/sunit/upstox/feature/portfolio/ui/PortfolioScreen.kt
@@ -0,0 +1,12 @@
+package com.sunit.upstox.feature.portfolio.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PortfolioScreen(
+    modifier: Modifier = Modifier
+) {
+    Text(text = "Hello Portfolio")
+}
